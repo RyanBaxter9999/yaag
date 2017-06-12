dea76a880d824899352b60e25673369f548e2dea
RyanBaxter9999 <ryanbaxter@outlook.com>
2017-06-12 16:57:18 -0600

    Create filter.go

--- filters/filter.go
+++ filters/filter.go
@@ -3,9 +3,9 @@ package filters
 import (
 	"encoding/json"
 	"encoding/xml"
-	"github.com/betacraft/yaag/middleware"
-	"github.com/betacraft/yaag/yaag"
-	"github.com/betacraft/yaag/yaag/models"
+	"github.com/ryanbaxter9999/yaag/middleware"
+	"github.com/ryanbaxter9999/yaag/yaag"
+	"github.com/ryanbaxter9999/yaag/yaag/models"
 	"github.com/revel/revel"
 	"log"
 	"net/http/httptest"
