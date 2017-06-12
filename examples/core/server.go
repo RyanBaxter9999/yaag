410521e7c21394f2ea834cc5e75ef6b497b195f4
RyanBaxter9999 <ryanbaxter@outlook.com>
2017-06-12 16:56:16 -0600

    Create server.go

--- examples/core/server.go
+++ examples/core/server.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/betacraft/yaag/middleware"
-	"github.com/betacraft/yaag/yaag"
+	"github.com/ryanbaxter9999/yaag/middleware"
+	"github.com/ryanbaxter9999/yaag/yaag"
 	"io/ioutil"
 	"net/http"
 	"strings"
